d19973ae4a70d0e65473914b91e1a3ad01f9322d
jiangchao828 <jiangchao828@163.com>
2020-12-15 08:54:00 +0800

    wwwwwww

--- b2c/b2c-registry/src/main/java/com/b2c/RegistryApplication.java
+++ b2c/b2c-registry/src/main/java/com/b2c/RegistryApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class RegistryApplication {
     public static void main(String[] args) {
+        System.out.println("");
         SpringApplication.run(RegistryApplication.class);
     }
 }
